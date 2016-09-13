9cd966131afcd15e72494bdc8ea143c4b6f338af
NIKITA ROBERTSON <nikitarobertson@adelphi.internal>
2016-09-13 12:08:27 -0400

    progress

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -1,11 +1,73 @@
 
 /**
- * Write a description of class PersonCreator here.
+ * PersonCreator models a person class that has a name, age, sex, height, 
+ * weight, social security, address, phone number, and date of birth.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (nikkirobertson) 
+ * @version (2016.09.13)
  */
 public class PersonCreator
 {
-    // Your class begins here!
+    // The name of the person.
+    private String name;
+    // The age of the person.
+    private int age;
+    // The sex of the person.
+    private String sex;
+    // The height of the person.
+    private int height;
+    // The weight of the person.
+    private double weight;
+    // The social security number
+    private int socialSecurity;
+    //The address of the person.
+    private String address;
+    // The phone number of the person.
+    private int phoneNumber;
+    //The date of birth of the person.
+    private int date;
+/**
+ * Create a person that requires a first and last name.
+ */ 
+public PersonCreator (String firstName, String lastName)
+{
+    name = firstName +" "+ lastName;
+   
 }
+    
+/**
+ * @Assign values to attributes.
+ */
+
+public void setAge (int newAge)
+{
+    age = newAge;
+}
+public void setSex (String newSex)
+{
+    sex = newSex;
+}
+public void setHeight (int newHeight) 
+{
+    height = newWeight;
+}
+public void setWeight (int newWeight)
+{
+    weight = newWeight;
+}
+public void socialSecuritynumber (String SSN)
+{
+    socialSecurity = newsocialSecurity;
+}  
+public void setAddress (String newAddress) 
+{
+    address = newAddress;
+}
+public void phoneNumber (int Var1,int Var2, int Var3)
+{
+    if(Var1 <100 { 
+    
+}
+public dateofBirth (String month, int date, int year)
+{
+    dateofBirth = 
