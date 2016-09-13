398051cdae5d5feef99f29f494b5523005778b42
Alex Blackwood <alexblackwood@adelphi.internal>
2016-09-13 12:05:54 -0400

    lab05 WIP

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -8,4 +8,23 @@
 public class PersonCreator
 {
     // Your class begins here!
+    private String name;
+    private int age;
+    private String sex;
+    private int height;
+    private int weight;
+    private String ssn;
+    private String address;
+    private int phoneNumber;
+    private String dob;
+    
+    public PersonCreator(String first, String last)
+    {
+        name = first + " " + last;
+    }
+    
+    
+    
+    
+    
 }
