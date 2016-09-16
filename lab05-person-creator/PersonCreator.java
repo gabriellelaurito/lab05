b82bc693e70067a22936f66a4a7b2b2c8f23917d
Alex <Alex@Alexs-MacBook-Pro.local>
2016-09-15 23:49:11 -0400

    Lab05 Update

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -1,8 +1,8 @@
 
 /**
  * Write a description of class PersonCreator here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class PersonCreator
@@ -17,28 +17,38 @@ public class PersonCreator
     private String address;
     private int phoneNumber;
     private String dob;
-    
+
     public PersonCreator(String first, String last)
     {
         name = first + " " + last;
     }
-    
+
     public void setAge(int newAge)
     {
         if (newAge > 10 && newAge < 99)
         {
-           age = newAge; 
+           age = newAge;
         }
-        
+
         else
         {
-          System.err.println("Please enter a valid age"); 
+          System.err.println("Please enter a valid age");
         }
-        
+
     }
-    
+
     public void setSex(String sex)
     {
         this.sex = sex;
+    }
+
+    public void setHeight(int newHeight)
+    {
+        height = newHeight;
+    }
+
+    public void setWeight(int newWeight)
+    {
+        weight = newWeight;
     }
 }
